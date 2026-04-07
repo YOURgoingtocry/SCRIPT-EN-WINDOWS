bda13aeb977acd53a450be3cb98c024adbcf7849
YOURgoingtocry <jonalestebanramirez17@gmail.com>
2026-04-07 13:18:36 -0400

    Add files via upload

---
+++ lab1/comandos-red.bat
@@ -0,0 +1,16 @@
+cls
+echo off
+echo bienvenido al script en bat con los comandos de red en windows
+ipconfig
+ipconfig/all
+ipconfig/displaydns
+ipconfig/flushdns
+ipconfig/release
+ipconfig/renew
+ping google.com
+ping facebook.com
+netstat -a
+getmac
+route print
+echo fin del programa en bat, muchas gracias por utilizarlo
+echo on
